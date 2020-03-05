3e80d29ae112669a3b65490a2fc38c415959cd8a
bruno.costa <bruno.costa@stormsec.com.br>
2020-03-04 23:40:02 -0300

    Updates

---
+++ precache-manifest.1ef29283b761ca87ea58e2f2e2c4b61f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2fe018a70973945b7e948fe938999a",
+    "url": "/alantechsafe/entre-amigos/index.html"
+  },
+  {
+    "revision": "155b3db314fd6246f7b7",
+    "url": "/alantechsafe/entre-amigos/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "155b3db314fd6246f7b7",
+    "url": "/alantechsafe/entre-amigos/static/js/2.544f2176.chunk.js"
+  },
+  {
+    "revision": "ada7f74f4e5976853df965c07cb4cc8f",
+    "url": "/alantechsafe/entre-amigos/static/js/2.544f2176.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9717a56ab08afd3e5ca",
+    "url": "/alantechsafe/entre-amigos/static/js/main.dc63ebe7.chunk.js"
+  },
+  {
+    "revision": "7c5307d17f9948051fce",
+    "url": "/alantechsafe/entre-amigos/static/js/runtime-main.d67e373b.js"
+  },
+  {
+    "revision": "279bc9160a7d29e901a10a28355da5bd",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_1.279bc916.png"
+  },
+  {
+    "revision": "a8a5600a6239c83e20e6b887ee6fa2b3",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_1_mobile.a8a5600a.png"
+  },
+  {
+    "revision": "122fc3c682ba21ea7414d38553bf86ee",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_2.122fc3c6.png"
+  },
+  {
+    "revision": "441ac5bf8a8af63a8462a5b96256e2bf",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_2_mobile.441ac5bf.png"
+  },
+  {
+    "revision": "9dff8e4c6f6d6f3772b82bef21a1dd9d",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_3.9dff8e4c.png"
+  },
+  {
+    "revision": "bb2589eeeb475a872be214d278bb8b06",
+    "url": "/alantechsafe/entre-amigos/static/media/carousel_3_mobile.bb2589ee.png"
+  },
+  {
+    "revision": "0436635dcc6f5b836747ff92bb8cb07c",
+    "url": "/alantechsafe/entre-amigos/static/media/events_parallax.0436635d.png"
+  },
+  {
+    "revision": "eb224b6b1d6babb39f5a6b063dad2275",
+    "url": "/alantechsafe/entre-amigos/static/media/home1.eb224b6b.png"
+  },
+  {
+    "revision": "9810351c95921f644d738f76cca4027a",
+    "url": "/alantechsafe/entre-amigos/static/media/home2.9810351c.png"
+  },
+  {
+    "revision": "54b78e41325f1099d1cb427bbeb355d9",
+    "url": "/alantechsafe/entre-amigos/static/media/logo.54b78e41.png"
+  },
+  {
+    "revision": "0ff543fa25bbb8cb0ae1e138cd5928fc",
+    "url": "/alantechsafe/entre-amigos/static/media/logo_footer.0ff543fa.png"
+  },
+  {
+    "revision": "2abc4dac5fcc871ccd83f04dc5a2e25c",
+    "url": "/alantechsafe/entre-amigos/static/media/logo_header.2abc4dac.png"
+  }
+]);
