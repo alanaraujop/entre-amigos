383142bbac36f71cc8ecbac367d0f9786a02591a
Yago Valuche <valuchedevs@gmail.com>
2021-01-27 17:15:35 -0300

    implementando getStaticProps na home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import BannerSlider from "../src/Components/BannerSlider";
+import { useState, useEffect } from "react";
+import { getCarousel } from "../src/services/carousel.service";
 
-export default function Home() {
+export default function Home({ slides }) {
   return (
     <div>
-      <BannerSlider />
+      <BannerSlider slides={slides} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const slides = await getCarousel();
+  return {
+    props: {
+      slides,
+    },
+  };
 }
