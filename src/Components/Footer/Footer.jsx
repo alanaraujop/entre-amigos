3a92d786b5770817a20d7124d9e00faa7f74a9e7
Yago Valuche <valuchedevs@gmail.com>
2021-02-17 17:05:46 -0300

    concluindo componente footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,42 +1,51 @@
-import FacebookIcon from "../../assets/images/facebook.png";
-// import InstagramIcon from "../../assets/images/instagram.png";
-// import FooterLogo from "../../assets/images/logo_footer.png";
 import Image from "next/image";
+import Link from "next/link";
+import * as S from "./Footer.styles";
+
 export function Footer() {
   return (
-    <div className="footer">
-      <div className="social-container">
-        <div className="divider" />
-        <span className="footer-title">Nos acompanhe aqui:</span>
-        <div className="social-icons">
-          <a
+    <S.ContainerFooter>
+      <S.SocialContainer>
+        <div />
+        <S.FooterTitle>Nos acompanhe aqui:</S.FooterTitle>
+        <S.ContainerSocialIcons>
+          <Link
             href="https://www.facebook.com/entreamigosgastronomia/"
-            target="_blank"
             rel="noopener noreferrer"
           >
-            {/* Imagem com erro no loader */}
-            <Image
-              src={FacebookIcon}
-              alt="Página no Facebook do Entre Amigos"
-              width={500}
-              height={500}
-            />
-          </a>
-          <a
+            <a target="_blank">
+              <Image
+                src="/assets/images/facebook.png"
+                alt="Página no Facebook do Entre Amigos"
+                width={32}
+                height={32}
+              />
+            </a>
+          </Link>
+          <Link
             href="https://www.instagram.com/entreamigos_rj/"
-            target="_blank"
             rel="noopener noreferrer"
           >
-            {/* <img src={InstagramIcon} alt="Instagram do Entre Amigos" /> */}
-          </a>
-        </div>
-        <span className="footer-name">Entre Amigos Gastronomia</span>
-      </div>
-      <div className="footer-logo">
-        {/* <img src={FooterLogo} alt="Logo do Entre Amigos Gastronomia" /> */}
-      </div>
-    </div>
+            <a target="_blank">
+              <Image
+                src="/assets/images/instagram.png"
+                alt="Instagram do Entre Amigos"
+                width={32}
+                height={32}
+              />
+            </a>
+          </Link>
+        </S.ContainerSocialIcons>
+        <S.FooterName>Entre Amigos Gastronomia</S.FooterName>
+      </S.SocialContainer>
+      <S.FooterLogo>
+        <Image
+          src="/assets/images/logo_footer.png"
+          alt="Logo do Entre Amigos Gastronomia"
+          width={80}
+          height={100}
+        />
+      </S.FooterLogo>
+    </S.ContainerFooter>
   );
 }
-
-export default Footer;
